7fd515b622f132ac32030d92d9b12c52138f275f
Rupali Kumari <rupalik.ug23.ma@nitp.ac.in>
2025-07-29 10:15:01 +0530

    initial_server_setup

---
+++ Server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Middleware to handle URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to handle CORS
+app.use(cors());
+// Middleware to serve static files
+app.use(express.static("public"));
+
+// Sample route to test the server
+app.get("/", (req, res) => {
+  res.send("Welcome to the PolicyPal Server!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
